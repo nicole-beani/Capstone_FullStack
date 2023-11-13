94c9b49b2350e77979222786ae84130c98481fcc
nicole beani <beani.nicole@gmail.com>
2023-11-14 00:24:04 +0100

    00:24

---
+++ Controllers/DetPrenotazioneController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WaveTheCave.Models;
+
+namespace WaveTheCave.Controllers
+{
+    public class DetPrenotazioneController : Controller
+    {
+    private ModelDBContext db = new ModelDBContext();
+        // GET: DetPrenotazione
+        public ActionResult ResocontoDetPrenotazione()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.IdGrotte = new SelectList(db.Grotte, "IdGrotte", "Nome");
+            
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdDetPrenotazione,Quantita, IdGrotte")] DetPrenotazione dp)
+        {
+            if (ModelState.IsValid)
+            {
+                db.DetPrenotazione.Add(dp);
+                db.SaveChanges();
+                return RedirectToAction("ResocontoDetPrenotazione", "DetPrenotazione");
+            }
+
+            return View(dp);
+        }
+    }
+}
